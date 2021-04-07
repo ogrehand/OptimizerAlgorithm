4b300ea03d9d7899f8c8dd9c35dc688e6e82f3be
tobings <tobingsetyawan@gmail.com>
2021-04-07 16:11:16 +0700

    fix gitignore

---
+++ PSO/Particle.py
@@ -0,0 +1,13 @@
+class Particle:
+    def __init__(self):
+        pass
+
+    def updateV(self):
+        pass
+
+    def updatePos(self):
+        pass
+
+    @staticmethod
+    def generaterandom():
+        return Particle()
